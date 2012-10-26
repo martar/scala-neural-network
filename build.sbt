c6db9c402e73b2aae387c98c5c360cfdc20c90b2
Kamil Figiela <kamil.figiela@gmail.com>
2012-10-26 18:32:31 +0200

    Update

---
+++ build.sbt
@@ -0,0 +1,4 @@
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.10" % "test"
+
--- src/main/scala/nnetworks/NNetwork.scala
+++ src/main/scala/nnetworks/NNetwork.scala
@@ -7,7 +7,7 @@ object Functions {
 /**
  * layer is a two dimensional array of doubles - weights
  */
-class Layer(weights: Array[Array[Double]]) {
+class Layer(activation: (Double) => Double, weights: Array[Array[Double]]) {
 
   /**
    * Fastest implementation based on http://blog.scala4java.com/2011/12/matrix-multiplication-in-scala-single.html
@@ -23,12 +23,19 @@ class Layer(weights: Array[Array[Double]]) {
     res
   }
 
-  def applyInputs(input: Array[Double], fun: Double => Double): Array[Double] = {
-    multiThreadedIdiomatic(input, weights) map (fun(_))
+  def eval(input: Array[Double]): Array[Double] = {
+    multiThreadedIdiomatic(input, weights) map (activation)
   }
+}
 
-  def applyInputs(input: Array[Double]): Array[Double] = {
-    multiThreadedIdiomatic(input, weights)
+
+class Network(layers: List[Layer]) {
+  def add(l: Layer) = new Network(l :: layers)
+  def eval(input: Array[Double]): Array[Double] = {
+    def eval0(layers: List[Layer], input: Array[Double]): Array[Double] = layers match {
+      case Nil => input
+      case head :: tail => eval0(tail, head.eval(input))
+    }
+    eval0(layers, input)
   }
-
 }
